12d63f497cef5623a3cb352ea92b19fa1b79c882
TenIke <anthonydzani@gmail.com>
2023-04-25 04:34:38 -0700

    main.h

--- main.h
+++ main.h
@@ -40,7 +40,7 @@ struct fmt
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i,
+int handle_print(const char *fmt, int *a,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /****************** FUNCTIONS ******************/
@@ -79,10 +79,10 @@ int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
 /* Funciotns to handle other specifiers */
-int get_flags(const char *format, int *i);
-int get_width(const char *format, int *i, va_list list);
-int get_precision(const char *format, int *i, va_list list);
-int get_size(const char *format, int *i);
+int get_flags(const char *format, int *a);
+int get_width(const char *format, int *a, va_list list);
+int get_precision(const char *format, int *a, va_list list);
+int get_size(const char *format, int *a);
 
 /*Function to print string in reverse*/
 int print_reverse(va_list types, char buffer[],
